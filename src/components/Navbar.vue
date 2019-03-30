8e7e3332d5d1051717c14dda16881f87a408856c
agnesrm <agnesr@stud.ntnu.no>
2019-03-30 15:09:11 +0100

    setup frontend

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-nav >
+        <v-toolbar app class="middel">
+
+            <v-toolbar-title class="text-uppercase grey--text">Dementia</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+
+            <v-btn flat color="grey">
+                <span> Logg ut</span>
+                <v-icon>exit_to_app</v-icon>
+            </v-btn>
+
+        </v-toolbar>
+    </v-nav>
+</template>
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
+import 'vuetify/src/stylus/app.styl'
+
+Vue.use(Vuetify, {
+  iconfont: 'md',
+  theme: {
+    hvit: "#f7f9fb",
+    lys:'#8fc1e3',
+    middel: '#5085a5',
+    mork: '#31708E',
+    gronn: '#687864'
+  }
+})
+
